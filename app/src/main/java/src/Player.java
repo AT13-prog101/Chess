ac52989f0e7fdddc486f1c995ce998a3923ed8b9
Jorge Caceres <jcaceresvelasco@gmail.com>
2021-03-10 22:03:35 -0400

    Add class Player for chess

---
+++ app/src/main/java/src/Player.java
@@ -0,0 +1,19 @@
+package src;
+
+import java.util.ArrayList;
+
+public class Player {
+    String name;
+    boolean playerTurn;
+    String color;
+    Player(String name, String color){
+        this.name = name;
+        this.color = color;
+    }
+    void makeMove(String move){
+
+    }
+    void moveFromTo(String move){
+
+    }
+}
