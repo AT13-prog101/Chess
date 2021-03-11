39ffd259cde12aee62e399418af3cf8e9ab4b448
Paola-Xaq <paola.xaq@gmail.com>
2021-03-10 22:38:32 -0400

    Update methods from abstract class Piece

--- app/src/main/java/src/Piece.java
+++ app/src/main/java/src/Piece.java
@@ -4,10 +4,8 @@ public abstract class Piece {
     private boolean isWhite;
     private int initPosX;
     private int initPosY;
-    private int finPosX;
-    private int finPosY;
     private boolean isCaptured;
-    //private Move move;
+    //private MovePiece move;
 
     public Piece(boolean white, int initPosX, int initPosY) {
         this.isWhite = white;
@@ -16,19 +14,66 @@ public abstract class Piece {
         this.initPosY = initPosY;
     }
 
-    public void setCaptured(boolean isCaptured) {
+    /**
+     * Set boolean value to isCaptured, when a piece is captured or not
+     * @param captured
+     * @return isCaptured
+     */
+    public void setCaptured(boolean captured) {
         this.isCaptured = isCaptured;
     }
 
+    /**
+     * Return state of isCaptured
+     * @return isCaptured
+     */
     public boolean isCaptured() {
         return this.isCaptured;
     }
 
+    /**
+     * Return initial position in X of Piece
+     * @return initPosX
+     */
     public int getInitPosX() {
         return initPosX;
     }
 
+    /**
+     * Set initial position in X of Piece when the piece is moved
+     * @param initPosX
+     */
+    public void setInitPosX(int initPosX) {
+        this.initPosX = initPosX;
+    }
+
+    /**
+     * Return initial position in Y of Piece
+     * @return initPosY
+     */
     public int getInitPosY() {
         return initPosY;
     }
+
+    /**
+     * Set initial position in Y of Piece when the piece is moved
+     * @param initPosY
+     */
+    public void setInitPosY(int initPosY) {
+        this.initPosY = initPosY;
+    }
+
+    /**
+     * Control if the moved made by the piece is valid or not
+     * @return true when position is valid.
+     */
+    /* public boolean isValidMove(int endPosX, int endPosY) {
+        if (move(endPosX, endPosY)) {
+            this.initPosX = endPosX;
+            this.initPosY = endPosY;
+            return true;
+        }
+        return false;
+    }
+    */
 }
