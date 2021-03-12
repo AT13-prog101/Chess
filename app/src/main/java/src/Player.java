afba12d1a9cf60115eea4d071879f31e619881f3
Jorge Caceres <jcaceresvelasco@gmail.com>
2021-03-11 22:35:28 -0400

    Add methods to validateInputs, possibleMovements, and sourceToTargetMovement

--- app/src/main/java/src/Player.java
+++ app/src/main/java/src/Player.java
@@ -1,19 +1,49 @@
 package src;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Player {
     String name;
     boolean playerTurn;
     String color;
-    Player(String name, String color){
+    Player (String name, String color){
         this.name = name;
         this.color = color;
     }
-    void makeMove(String move){
+    void makeMove(){
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Insert the movement:");
+        String movement =scan.next();
+        if (movement.isEmpty()) {
+            invalidMessage();
+            makeMove();
+        }
+        char[] inputArray= movement.toCharArray();
+        validateInput(inputArray);
 
     }
-    void moveFromTo(String move){
+
+    void validateInput(char[] playerInput){
+
+        if (playerInput[0] =='S'){
+            char[] source = new char[]{playerInput[2],playerInput[3]};
+            char[] target = new char[]{playerInput[7],playerInput[8]};
+            sourceToTargetMovement(source,target);
+        }else {
+            char[] piece = new char[]{playerInput[2],playerInput[3]};
+            possibleMovements(piece);
+        }
+    }
+    void invalidMessage(){
+        System.out.println("The inserted info is not valid");
+    }
+    void sourceToTargetMovement(char[] source,char[] target){
+
+    }
+    void possibleMovements(char[] piece){
+
 
     }
 }
+
