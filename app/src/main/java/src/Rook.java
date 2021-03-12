13d6827016edc09d02ecf217a005095692b22a7f
Gustavo Huanca <gustavozha@gmail.com>
2021-03-11 21:25:20 -0400

    Horse, Pawn and Rook extends Piece

--- app/src/main/java/src/Rook.java
+++ app/src/main/java/src/Rook.java
@@ -1,5 +1,9 @@
 package src;
 
-public class Rook {
+public class Rook extends Piece{
     private char figure = 'R';
+
+    public Rook(boolean white, int initPosX, int initPosY) {
+        super(white, initPosX, initPosY);
+    }
 }
