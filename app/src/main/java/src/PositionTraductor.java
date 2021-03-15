adf49a978d96de17ecc3d7df1458a117a1f08cc3
SaulCM <saulcaspamiranda@gmail.com>
2021-03-14 23:41:48 -0400

    fixed mismatch for rows

--- app/src/main/java/src/PositionTraductor.java
+++ app/src/main/java/src/PositionTraductor.java
@@ -3,6 +3,22 @@ package src;
 public class PositionTraductor {
     private final int ASCII_CODE_FOR_A = 97;
     private final int ASCII_CODE_FOR_0 = 48;
+    private final int NUMBER_OF_ROWS = 8;
+    /**
+     * PositionTraductor switches from one notation to the other.
+     *
+     *               ALGEBRAIC                         MATRIX
+     *      8| BR BH BB BQ BK BB BH BR      0| BR BH BB BQ BK BB BH BR
+     *      7| BP BP BP BP BP BP BP BP      1| BP BP BP BP BP BP BP BP
+     *      6|                              2|
+     *      5|                              3|
+     *      4|                          ==> 4|
+     *      3|                              5|
+     *      2| WP WP WP WP WP WP WP WP      6| WP WP WP WP WP WP WP WP
+     *      1| WR WH WB WQ WK WB WH WR      7| WR WH WB WQ WK WB WH WR
+     *        ------------------------         -----------------------
+     *         a  b  c  d  e  f  g  h          0  1  2  3  4  5  6  7
+     */
     public PositionTraductor() {
     }
 
@@ -12,11 +28,11 @@ public class PositionTraductor {
      * */
     public int[] getIndexNotation(String algebraicNotation) {
         int[] positions = new int[2];
-        //Chars received represents the column and row, will be cast to int and  will be subtracted its value
+        //Chars received represents the column and row, will be cast to int and  will be subtracted its ascii value
         char characterCol = algebraicNotation.charAt(0);
         char characterRow = algebraicNotation.charAt(1);
         positions[0] = (int)characterCol - ASCII_CODE_FOR_A;
-        positions[1] = (int)characterRow - ASCII_CODE_FOR_0 - 1;
+        positions[1] = Math.abs(((int)characterRow - ASCII_CODE_FOR_0) - NUMBER_OF_ROWS);
         return positions;
     }
 
@@ -24,9 +40,12 @@ public class PositionTraductor {
      * Given 2 values that represent the row and col positions in a matrix. Return the corresponding
      * algebraic notation for that position
      */
-    public String getAlgebraicNotation(int rowIndex, int colIndex) {
-        String algebraicNotation = String.valueOf(rowIndex + 1);
-        algebraicNotation += (char)(colIndex + 97);
+    public String getAlgebraicNotation(int colIndex, int rowIndex) {
+        //Adding column value in algebraic notation
+        String algebraicNotation = String.valueOf((char)(colIndex + 97));
+        //2nd value is row position
+        int rowAlgebraic = NUMBER_OF_ROWS - rowIndex;
+        algebraicNotation += String.valueOf(rowAlgebraic);
         return algebraicNotation;
     }
 }
--- app/src/test/java/src/PositionTraductorTest.java
+++ app/src/test/java/src/PositionTraductorTest.java
@@ -5,27 +5,59 @@ import static org.junit.Assert.assertEquals;
 
 public class PositionTraductorTest {
     @Test
-    public void getIndexNotationGivenA1Return00() {
+    public void getIndexNotationGivena1Return00() {
         PositionTraductor positionTraductor = new PositionTraductor();
         String expected = "00";
-        int[] indexNotation = positionTraductor.getIndexNotation("a1");
+        int[] indexNotation = positionTraductor.getIndexNotation("a8");
         String actual = String.valueOf(indexNotation[0]) + String.valueOf(indexNotation[1]);
         assertEquals(expected, actual);
     }
     @Test
-    public void getIndexNotationGivenC5Return24() {
+    public void getIndexNotationGivenc5Return24() {
         PositionTraductor positionTraductor = new PositionTraductor();
         String expected = "24";
-        int[] indexNotation = positionTraductor.getIndexNotation("c5");
+        int[] indexNotation = positionTraductor.getIndexNotation("c4");
         String actual = String.valueOf(indexNotation[0]) + String.valueOf(indexNotation[1]);
         assertEquals(expected, actual);
     }
     @Test
-    public void getIndexNotationGivenH8Return77() {
+    public void getIndexNotationGivenh8Return77() {
         PositionTraductor positionTraductor = new PositionTraductor();
         String expected = "77";
-        int[] indexNotation = positionTraductor.getIndexNotation("h8");
+        int[] indexNotation = positionTraductor.getIndexNotation("h1");
         String actual = String.valueOf(indexNotation[0]) + String.valueOf(indexNotation[1]);
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void getAlgebraicNotationGiven00Returna1() {
+        PositionTraductor positionTraductor = new PositionTraductor();
+        String expected = "a1";
+        String indexNotation = positionTraductor.getAlgebraicNotation(0, 7);
+        String actual = indexNotation;
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void getAlgebraicNotationGiven44Returnc3() {
+        PositionTraductor positionTraductor = new PositionTraductor();
+        String expected = "c5";
+        String indexNotation = positionTraductor.getAlgebraicNotation(2, 3);
+        String actual = indexNotation;
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void getAlgebraicNotationGiven46Returnd7() {
+        PositionTraductor positionTraductor = new PositionTraductor();
+        String expected = "d7";
+        String indexNotation = positionTraductor.getAlgebraicNotation(3, 1);
+        String actual = indexNotation;
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void getAlgebraicNotationGiven77Returnh7() {
+        PositionTraductor positionTraductor = new PositionTraductor();
+        String expected = "h8";
+        String indexNotation = positionTraductor.getAlgebraicNotation(7, 0);
+        String actual = indexNotation;
         assertEquals(expected, actual);
     }
 }
