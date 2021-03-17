68bb69fe0df92dd20c4f8eac819deb216250d928
Gustavo Huanca <gustavozha@gmail.com>
2021-03-17 02:30:15 -0400

    Implement rule for LineMove

--- app/src/main/java/src/LinedMove.java
+++ app/src/main/java/src/LinedMove.java
@@ -1,8 +1,27 @@
 package src;
 
 public class LinedMove implements MovePiece{
+    public final static int DIMENSION = 8;
     @Override
     public void move(int posX, int posY, boolean isWhite) {
-        
+        boolean killPiece=!isWhite;
+        //Down
+        for (int i = posX; i < DIMENSION; i++) {
+            //board[i][posY] = "X";
+        }
+        //Up
+        for (int i = 0; i < posX; i++) {
+            //board[i][posY] = "X";
+        }
+        //right
+        for (int i = posY; i < DIMENSION; i++) {
+            //board[posX][i] = "X";
+        }
+        //Left
+        for (int i = 0; i < posY; i++) {
+            //board[posX][i] = "X";
+        }
+        //The piece doesn't occupy its position again
+        //board[posX][posY] = "-";
     }
 }
