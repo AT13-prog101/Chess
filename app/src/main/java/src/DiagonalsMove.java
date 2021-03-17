f7b3ef792103b385404a6bb7bcfaa936842abd8c
Gustavo Huanca <gustavozha@gmail.com>
2021-03-17 02:34:33 -0400

    Implement rule for DiagonalsMove

--- app/src/main/java/src/DiagonalsMove.java
+++ app/src/main/java/src/DiagonalsMove.java
@@ -3,6 +3,28 @@ package src;
 public class DiagonalsMove implements MovePiece{
     @Override
     public void move(int posX, int posY, boolean isWhite) {
+        int dimension = 8;
+        int numerMax=Math.max(posX,posY);
+        int numerMin=Math.min(posX,posY);
+        //cuadrante IV, (I)
+        for (int i = 1; numerMax + i < dimension; i++) {
+            //board[posX + i][posY + i] = "X";
+        }
+        //cuadrante II (III)
+        for (int i = 1; numerMin - i >=0; i++) {
+            //board[posX - i][posY - i] = "X";
+        }
 
+        //cuadrante III (IV)
+        for (int i = 1; posX + i < dimension && posY - i >= 0; i++) {
+            //board[posX + i][posY - i] = "X";
+        }
+
+        //cuadrante I (II)
+        for (int i = 1; posY + i < dimension && posX - i >= 0; i++) {
+            //board[posX -i][posY + i] = "X";
+        }
+        //The piece doesn't occupy its position again
+        //board[posX][posY] = " ";
     }
 }
--- app/src/main/java/src/LinedMove.java
+++ app/src/main/java/src/LinedMove.java
@@ -1,12 +1,13 @@
 package src;
 
 public class LinedMove implements MovePiece{
-    public final static int DIMENSION = 8;
+
     @Override
     public void move(int posX, int posY, boolean isWhite) {
+        int dimension = 8;
         boolean killPiece=!isWhite;
         //Down
-        for (int i = posX; i < DIMENSION; i++) {
+        for (int i = posX; i < dimension; i++) {
             //board[i][posY] = "X";
         }
         //Up
@@ -14,7 +15,7 @@ public class LinedMove implements MovePiece{
             //board[i][posY] = "X";
         }
         //right
-        for (int i = posY; i < DIMENSION; i++) {
+        for (int i = posY; i < dimension; i++) {
             //board[posX][i] = "X";
         }
         //Left
@@ -22,6 +23,6 @@ public class LinedMove implements MovePiece{
             //board[posX][i] = "X";
         }
         //The piece doesn't occupy its position again
-        //board[posX][posY] = "-";
+        //board[posX][posY] = " ";
     }
 }
