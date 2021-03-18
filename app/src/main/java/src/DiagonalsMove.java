d95ff671b40e1a6a3234aa895ca56b8fb0db965f
Gustavo Huanca <gustavozha@gmail.com>
2021-03-18 09:12:08 -0400

    Refactor DiagonalMone and linedMove

--- app/src/main/java/src/DiagonalsMove.java
+++ app/src/main/java/src/DiagonalsMove.java
@@ -1,30 +1,102 @@
 package src;
 
-public class DiagonalsMove implements MovePiece{
-    @Override
-    public void move(int posX, int posY, boolean isWhite) {
-        int dimension = 8;
-        int numerMax=Math.max(posX,posY);
-        int numerMin=Math.min(posX,posY);
-        //cuadrante IV, (I)
-        for (int i = 1; numerMax + i < dimension; i++) {
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DiagonalsMove implements MovePiece {
+    List<Point> movePoints = new ArrayList<Point>();
+
+    public void move(int posX, int posY, Piece piece) {
+        if (isMoveValid(posX, posY)) {
+            piece.setPosY(posY);
+            piece.setPosX(posX);
+            movePoints = new ArrayList<Point>();
+        }
+    }
+
+    public void possibleMoves(Piece[][] board, Piece piece) {
+        int posX = piece.getInitPosX();
+        int posY = piece.getInitPosY();
+        int numerMax = Math.max(posX, posY);
+        int numerMin = Math.min(posX, posY);
+        boolean killPiece = !piece.getColorWhite();
+        for (int i = 1; numerMax + i < board.length; i++) {
             //board[posX + i][posY + i] = "X";
+            if (board[posX + i][posY + i] == null) {
+                movePoints.add(new Point(posX + i, posY + i));
+            }
+            if (board[posX + i][posY + i].getColorWhite() == killPiece) {
+                movePoints.add(new Point(posX + i, posY + i));
+                break;
+            }
+            if (board[posX + i][posY + i].getColorWhite() == piece.getColorWhite()) {
+                break;
+            }
         }
         //cuadrante II (III)
-        for (int i = 1; numerMin - i >=0; i++) {
+        for (int i = 1; numerMin - i >= 0; i++) {
             //board[posX - i][posY - i] = "X";
+            if (board[posX - i][posY - i] == null) {
+                movePoints.add(new Point(posX - i, posY - i));
+            }
+            if (board[posX - i][posY - i].getColorWhite() == killPiece) {
+                movePoints.add(new Point(posX - i, posY - i));
+                break;
+            }
+            if (board[posX - i][posY - i].getColorWhite() == piece.getColorWhite()) {
+                break;
+            }
         }
 
         //cuadrante III (IV)
-        for (int i = 1; posX + i < dimension && posY - i >= 0; i++) {
+        for (int i = 1; posX + i < board.length && posY - i >= 0; i++) {
             //board[posX + i][posY - i] = "X";
+            if (board[posX + i][posY - i] == null) {
+                movePoints.add(new Point(posX + i, posY - i));
+            }
+            if (board[posX + i][posY - i].getColorWhite() == killPiece) {
+                movePoints.add(new Point(posX + i, posY - i));
+                break;
+            }
+            if (board[posX + i][i].getColorWhite() == piece.getColorWhite()) {
+                break;
+            }
         }
 
         //cuadrante I (II)
-        for (int i = 1; posY + i < dimension && posX - i >= 0; i++) {
+        for (int i = 1; posY + i < board.length && posX - i >= 0; i++) {
             //board[posX -i][posY + i] = "X";
+            if (board[posX - i][posY + i] == null) {
+                movePoints.add(new Point(posX - i, posY + i));
+            }
+            if (board[posX - i][posY + i].getColorWhite() == killPiece) {
+                movePoints.add(new Point(posX - i, posY + i));
+                break;
+            }
+            if (board[posX - i][posY + i].getColorWhite() == piece.getColorWhite()) {
+                break;
+            }
         }
-        //The piece doesn't occupy its position again
-        //board[posX][posY] = " ";
+    }
+
+    public boolean isMoveValid(int xDest, int yDest) {
+        for (Point availableMove : movePoints) {
+            if (availableMove.getX() == xDest && availableMove.getY() == yDest)
+                return true;
+        }
+        return false;
+    }
+
+    /**
+     * Verify if the space of destination is free from piece of the same color
+     *
+     * @param xPosition
+     * @param yPosition
+     * @param isWhite
+     * @return
+     */
+    public boolean isSpaceEmpty(int xPosition, int yPosition, boolean isWhite) {
+        return true;
     }
 }
--- app/src/main/java/src/MovePiece.java
+++ app/src/main/java/src/MovePiece.java
@@ -1,5 +1,5 @@
 package src;
 
 public interface MovePiece {
-    void move(int posX,int posY, boolean isWhite);
+    void move(int posX, int posY, Piece piece);
 }
