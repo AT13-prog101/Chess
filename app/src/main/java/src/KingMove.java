821ff2e892f6ae94fa9eb22030e3a3d30d95d116
Gustavo Huanca <gustavozha@gmail.com>
2021-03-19 11:27:45 -0400

    KingMove implements MovePiece

--- app/src/main/java/src/KingMove.java
+++ app/src/main/java/src/KingMove.java
@@ -1,4 +1,17 @@
 package src;
 
-public class KingMove {
+import src.Pieces.Piece;
+
+import java.util.ArrayList;
+
+public class KingMove implements MovePiece{
+    @Override
+    public void move(int posX, int posY, Piece piece) {
+
+    }
+
+    @Override
+    public ArrayList<String> getValidMoves(String position) {
+        return null;
+    }
 }
