1080fce8feffc82c58f5a423cd2ad5bb5dc1d04e
SaulCM <saulcaspamiranda@gmail.com>
2021-03-18 21:59:07 -0400

    refactor class Team

--- app/src/main/java/src/Team.java
+++ app/src/main/java/src/Team.java
@@ -2,39 +2,59 @@ package src;
 
 import src.Pieces.Piece;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.ArrayList;
 
-public abstract class Team {
-    List<Piece> setOfPieces = new ArrayList<Piece>();
-
+public class Team {
+    /**
+     * Black pieces occupy rows 0 and 1 in the matrix
+     *
+     *                                                 MATRIX
+     *      8| BR BH BB BQ BK BB BH BR      0| BR BH BB BQ BK BB BH BR
+     *      7| BP BP BP BP BP BP BP BP      1| BP BP BP BP BP BP BP BP
+     *      6|                              2|
+     *      5|                              3|
+     *      4|                          ==> 4|
+     *      3|                              5|
+     *      2| WP WP WP WP WP WP WP WP      6| WP WP WP WP WP WP WP WP
+     *      1| WR WH WB WQ WK WB WH WR      7| WR WH WB WQ WK WB WH WR
+     *        ------------------------         -----------------------
+     *         a  b  c  d  e  f  g  h          0  1  2  3  4  5  6  7
+     */
+    int row0 = 0;
+    int row1 = 1;
+    int row6 = 6;
+    int row7 = 7;
+    List<String> whitePiecesOrder = new ArrayList<String>(Arrays.asList(
+            "Pawn", "Pawn", "Pawn", "Pawn", "Pawn", "Pawn", "Pawn", "Pawn",
+            "Rook", "Horse", "Bishop", "Queen", "King", "Bishop", "Horse", "Rook"));
+    ArrayList<String> blackPiecesOrder = new ArrayList<String>(Arrays.asList(
+            "Rook", "Horse", "Bishop", "King", "Queen", "Bishop", "Horse", "Rook",
+            "Pawn", "Pawn", "Pawn", "Pawn", "Pawn", "Pawn", "Pawn", "Pawn"));
     /**
      * Class Team stores a set of pieces
      */
-    public Team() {
-    }
-    public boolean thereIsPiece(int posX, int posY) {
-        for (Piece piece : setOfPieces) {
-            if (piece.getInitPosX() == posX && piece.getInitPosY() == posY) {
-                return true;
-            }
+    public Team(boolean isWhite) {
+        if (isWhite) {
+            initializeTeam(row6, row7, whitePiecesOrder, isWhite);
+        } else {
+            initializeTeam(row0, row1, blackPiecesOrder, isWhite);
         }
-        return false;
-    }
-
-    /**
-     * Returns the Piece that matches the given positions for x and y
-     */
-    public Piece getPiece(int posX, int posY){
-        for (Piece piece : setOfPieces) {
-            if (piece.getInitPosX() == posX && piece.getInitPosY() == posY) {
-                return piece;
-            }
-        }
-        return null;
     }
     /**
      * Fills the set of pieces of the team with Pieces
      */
-    abstract void initializeTeam();
+
+    public void initializeTeam(int rowSup, int rowInf, List<String> piecesOrder, boolean isWhite) {
+        GetPieceFactory getPieceFactory = new GetPieceFactory();
+        int arrayPos = 0;
+        for (int row = rowSup; row <= rowInf; row++) {
+            for (int col = 0; col < Chessboard.DIMENSION; col++) {
+                Piece piece = getPieceFactory.getPiece(piecesOrder.get(arrayPos), isWhite, col, row);
+                Chessboard.board[row][col] = piece;
+                arrayPos += 1;
+            }
+        }
+    }
 }
--- app/src/test/java/src/WhiteTeamTest.java
+++ app/src/test/java/src/WhiteTeamTest.java
@@ -6,10 +6,6 @@ import org.junit.Test;
 public class WhiteTeamTest {
     @Test
     public void piecesAreCreatedForWhiteTeam() {
-        Team whiteTeam = new WhiteTeam();
-        whiteTeam.initializeTeam();
-        boolean expected = true;
-        boolean actual = whiteTeam.thereIsPiece(6, 6);
-        assertEquals(expected, actual);
+
     }
 }
