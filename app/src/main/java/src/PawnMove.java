be2a921ca452017489aaa4159bbdc441c56473e2
Gustavo Huanca <gustavozha@gmail.com>
2021-03-20 23:11:41 -0400

    Add getValidMoves in PawnMove, but it doesnot implement yet

--- app/src/main/java/src/PawnMove.java
+++ app/src/main/java/src/PawnMove.java
@@ -6,37 +6,42 @@ import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
 
-public class PawnMove implements MovePiece{
+public class PawnMove implements MovePiece {
     List<Point> movePoints = new ArrayList<Point>();
 
     public void move(int posX, int posY, Piece piece) {
-        if(isMoveValid(posX, posY)) {
+        if (isMoveValid(posX, posY)) {
             piece.setPosY(posY);
             piece.setPosX(posX);
             piece.setIsMoved(true);
             movePoints = new ArrayList<Point>();
-            if(piece.getInitPosY() == 0 || piece.getInitPosY() < 8)
+            if (piece.getInitPosY() == 0 || piece.getInitPosY() < 8)
                 promotePawn();
         }
+    }
+
+    @Override
+    public List<Position> getValidMoves(Position position, boolean colorPiece) {
+        return null;
     }
 
     public void possibleMoves(int[][] chessBoard, Piece piece) {
         int oneMove = 1;
         int doubleMove = 2;
-        if(piece.getColorWhite()) {
+        if (piece.getColorWhite()) {
             oneMove = -1;
             doubleMove = -2;
         }
-        if(!piece.isMoved()) {
-            if(isSpaceEmpty(piece.getInitPosX(), piece.getInitPosY() + doubleMove, piece.getColorWhite()))
+        if (!piece.isMoved()) {
+            if (isSpaceEmpty(piece.getInitPosX(), piece.getInitPosY() + doubleMove, piece.getColorWhite()))
                 movePoints.add(new Point(piece.getInitPosX(), piece.getInitPosY() + doubleMove));
         }
-        if(0 <= (piece.getInitPosY() + oneMove) && (piece.getInitPosY() + oneMove) < chessBoard.length)
+        if (0 <= (piece.getInitPosY() + oneMove) && (piece.getInitPosY() + oneMove) < chessBoard.length)
             movePoints.add(new Point(piece.getInitPosX(), piece.getInitPosY() + oneMove));
     }
 
     public boolean isMoveValid(int xDest, int yDest) {
-        for (Point availableMove: movePoints) {
+        for (Point availableMove : movePoints) {
             if (availableMove.getX() == xDest && availableMove.getY() == yDest)
                 return true;
         }
@@ -52,6 +57,6 @@ public class PawnMove implements MovePiece{
     }
 
     public ArrayList<Position> getValidMoves(Position position) {
-        return  null;
+        return null;
     }
 }
