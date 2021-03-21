3edf60a47246d5213020fdbe3eb2bc7ef44cbd4d
Gustavo Huanca <gustavozha@gmail.com>
2021-03-20 22:49:29 -0400

    Refactor KingMove class

--- app/src/main/java/src/KingMove.java
+++ app/src/main/java/src/KingMove.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class KingMove implements MovePiece{
-    private List<Point> movePoints = new ArrayList<Point>();
+    private List<Position> movePoints = new ArrayList<Position>();
 
     /**
      * @param posX Destination X
@@ -15,37 +15,32 @@ public class KingMove implements MovePiece{
      * @param piece Piece Knight
      */
     public void move(int posX, int posY, Piece piece) {
-        possibleMoves(piece);
+        //possibleMoves(piece);
         if(isMoveValid(posX, posY)) {
             piece.setPosY(posY);
             piece.setPosX(posX);
         }
     }
 
-    @Override
-    public List<Position> getValidMoves(Position position) {
-        return null;
-    }
-
     /**
      * Return a List of Points with all possible moves of Knight
-     * @param piece the Knight piece
+     * @param colorPiece the Knight piece
      * @return movePoints List of Points of moves
      */
-    public List<Point> possibleMoves(Piece piece) {
+    public List<Position> getValidMoves(Position position, boolean colorPiece) {
 
         int[] xPossiblePos = {1, 1, -1, -1, 0, -1, 1, 0};
         int[] yPossiblePos = {1, -1, 1, -1, -1, 0, 0, 1};
 
-        int xActPos = piece.getInitPosX();
-        int yActPos = piece.getInitPosY();
+        int xActPos = position.getPosX();
+        int yActPos = position.getPosY();
 
         for(int i = 0; i < xPossiblePos.length ; i++) {
             int xPosition = xActPos + xPossiblePos[i];
             int yPosition = yActPos + yPossiblePos[i];
             if (0 <= xPosition && 0 <= yPosition &&xPosition < Chessboard.DIMENSION && yPosition < Chessboard.DIMENSION) {
-                if(isPositionAvailable(Chessboard.board[xPosition][yPosition], piece.getColorWhite())) {
-                    movePoints.add(new Point(xPosition, yPosition));
+                if(isPositionAvailable(Chessboard.board[xPosition][yPosition], colorPiece)) {
+                    movePoints.add(new Position(xPosition, yPosition));
                 }
             }
         }
@@ -58,8 +53,8 @@ public class KingMove implements MovePiece{
      * @return true if move is possible, false if move is no possible
      */
     private boolean isMoveValid(int xDest, int yDest) {
-        for (Point availableMove: movePoints) {
-            if (availableMove.getX() == xDest && availableMove.getY() == yDest)
+        for (Position availableMove: movePoints) {
+            if (availableMove.getPosX() == xDest && availableMove.getPosY() == yDest)
                 return true;
         }
         return false;
