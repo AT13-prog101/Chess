fab43e862b78b2fa0b4d103ee6b803fcf7ea1225
Gustavo Huanca <gustavozha@gmail.com>
2021-03-21 00:21:42 -0400

    Add setLife and getLife method and add move of piece

--- app/src/main/java/src/Pieces/King.java
+++ app/src/main/java/src/Pieces/King.java
@@ -1,7 +1,11 @@
 package src.Pieces;
 
+import src.KingMove;
+import src.KnightMove;
+
 public class King extends Piece {
     private char figure = 'K';
+    private boolean alive=true;
     public King(boolean white, int initPosX, int initPosY) {
         super(white, initPosX, initPosY);
         if (white == true) {
@@ -10,7 +14,25 @@ public class King extends Piece {
         {
             setColor('B');
         }
+        addMoveType(new KingMove());
     }
+
+    /**
+     *
+     * @return life of king
+     */
+    public boolean getAlive(){return alive;}
+
+    /** Change status life of king
+     *
+     * @return void
+     */
+    public void setAlive(boolean changeStatusLife){ this.alive=changeStatusLife;}
+
+    /** Return figure of king
+     *
+     * @return char that is figure of king
+     */
     public char getFigure() {
         return this.figure;
     }
