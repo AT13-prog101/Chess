67f53fa41ab21a32a0e03f4ccadeb35da48a712e
Gustavo Huanca <gustavozha@gmail.com>
2021-03-22 13:00:24 -0400

    Change refactir method makeAMove

--- app/src/main/java/src/MoveController.java
+++ app/src/main/java/src/MoveController.java
@@ -23,10 +23,11 @@ public class MoveController {
         this.target = target;
         this.player = player;
         //Search the target position in the list of valid moves of the piece to move
-            for (int i = 0; i < validMoves.size(); i++) {
-                Position pos = validMoves.get(i);
+           // for (int i = 0; i < validMoves.size(); i++) {
+                //Position pos = validMoves.get(i);
                 //if (target.getPosY() == pos.getPosY() && target.getPosX() == pos.getPosX()) {
-                if (target.equals(pos)) {
+                //if (target.equals(pos)) {
+                if(validMoves.contains(target)){
                     System.out.println("XXX");
                     //save reference of source piece
                     Piece pieceToMove = Chessboard.board[source.getPosY()][source.getPosX()];
@@ -42,7 +43,7 @@ public class MoveController {
                     //remove reference to piece from source position
                     Chessboard.board[source.getPosY()][source.getPosX()] = null;
                 }
-            }
+           // }
     }
 
     /**
