ff7e249816dcdf1249d78abc55eabdba54491ec6
Gustavo Huanca <gustavozha@gmail.com>
2021-03-22 01:07:47 -0400

    Refactor diagonalMOve

--- app/src/main/java/src/DiagonalsMove.java
+++ app/src/main/java/src/DiagonalsMove.java
@@ -5,71 +5,74 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DiagonalsMove implements MovePiece {
-    List<Position> movePoints = new ArrayList<Position>();
+    List<Position> movePositions = new ArrayList<>();
 
     @Override
     public List<Position> getPossibleMoves(Piece piece) {
-        int posX = piece.getInitPosX();
-        int posY = piece.getInitPosY();
-        int numberMax = Math.max(posX, posY);
-        int numberMin = Math.min(posX, posY);
+        int xPos = piece.getInitPosX();
+        int yPos = piece.getInitPosY();
+        int numberMax = Math.max(xPos, yPos);
+        int numberMin = Math.min(xPos, yPos);
         //southeast (SE)
         for (int i = 1; numberMax + i < Chessboard.DIMENSION; i++) {
-            if (!isAccessiblePosition(posX + i, posY + i, piece.getColorWhite())) {
+            if(isSpaceEmpty(Chessboard.board[yPos+i][xPos+i])) {
+                movePositions.add(new Position(xPos+i, yPos+i));
+            }else if(isSpaceWithEnemy(Chessboard.board[yPos+i][xPos+i], piece.getColorWhite())) {
+                movePositions.add(new Position(xPos+i, yPos+i));
                 break;
-            }
+            }else break;
         }
         //northwest (NW).
         for (int i = 1; numberMin - i >= 0; i++) {
-            if (!isAccessiblePosition(posX - i, posY - i, piece.getColorWhite())) {
+            if(isSpaceEmpty(Chessboard.board[yPos-1][xPos-i])) {
+                movePositions.add(new Position(xPos-i, yPos-1));
+            }else if(isSpaceWithEnemy(Chessboard.board[yPos-1][xPos-i], piece.getColorWhite())) {
+                movePositions.add(new Position(xPos-i, yPos-1));
                 break;
-            }
+            }else break;
         }
         //southwest (SW)
-        for (int i = 1; posX + i < Chessboard.DIMENSION && posY - i >= 0; i++) {
-            if (!isAccessiblePosition(posX + i, posY - i, piece.getColorWhite())) {
+        for (int i = 1; xPos + i < Chessboard.DIMENSION && yPos - i >= 0; i++) {
+            if(isSpaceEmpty(Chessboard.board[yPos-i][xPos+i])) {
+                movePositions.add(new Position(xPos+i, yPos-i));
+            }else if(isSpaceWithEnemy(Chessboard.board[yPos-i][xPos+i], piece.getColorWhite())) {
+                movePositions.add(new Position(xPos+i, yPos-i));
                 break;
-            }
+            }else break;
         }
         //northeast (NE)
-        for (int i = 1; posY + i < Chessboard.DIMENSION && posX - i >= 0; i++) {
-            if (!isAccessiblePosition(posX - i, posY + i, piece.getColorWhite())) {
+        for (int i = 1; yPos + i < Chessboard.DIMENSION && xPos - i >= 0; i++) {
+            if(isSpaceEmpty(Chessboard.board[yPos+i][xPos-i])) {
+                movePositions.add(new Position(xPos-i, yPos+i));
+            }else if(isSpaceWithEnemy(Chessboard.board[yPos+i][xPos-i], piece.getColorWhite())) {
+                movePositions.add(new Position(xPos-i, yPos+i));
                 break;
-            }
+            }else break;
         }
-        return movePoints;
+        return movePositions;
     }
 
-    public boolean isMoveValid(int xDest, int yDest) {
-        for (Position availableMove : movePoints) {
-            if (availableMove.getPosX() == xDest && availableMove.getPosY() == yDest)
-                return true;
-        }
+    /**
+     *  Verify if the space of destination is free from piece of the same color or is empty.
+     * @param piece is the piece in the possible position.
+     * @return true is space is empty or has a piece of different color, false if has piece of same color
+     */
+    public boolean isSpaceEmpty(Piece piece) {
+        if(piece == null)
+            return true;
         return false;
     }
 
     /**
-     * Verify if the space of destination is free from piece of the same color
-     *
-     * @param posX is a position analise of Chessboard
-     * @param posY is a position analise of Chessboard
-     * @return boolean which say is accessible Position
+     *  Verify if Destiny position has an enemy
+     * @param piece
+     * @param isWhite
+     * @return
      */
-    public boolean isAccessiblePosition(int posX, int posY, boolean colorPiece) {
-        boolean oppositeColor = !colorPiece;
-
-        if (Chessboard.board[posX][posY] == null) {
-            movePoints.add(new Position(posX, posY));
+    public boolean isSpaceWithEnemy(Piece piece, boolean isWhite) {
+        if(piece.getColorWhite() != isWhite)
             return true;
-        }
-        if (Chessboard.board[posX][posY].getColorWhite() != oppositeColor) {
-            movePoints.add(new Position(posX, posY));
+        else
             return false;
-        }
-        if (Chessboard.board[posX][posY].getColorWhite() == colorPiece) {
-            return false;
-        }
-        return false;
     }
 }
-
