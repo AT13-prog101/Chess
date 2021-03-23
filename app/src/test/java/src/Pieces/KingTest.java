5b7a00672c5c3160a6ae4a17d5280fe7a1514658
Gustavo Huanca <gustavozha@gmail.com>
2021-03-22 21:35:59 -0400

    Add unit test kingIsWhite_inputTrue_W for KingTest

--- app/src/test/java/src/Pieces/KingTest.java
+++ app/src/test/java/src/Pieces/KingTest.java
@@ -7,6 +7,16 @@ import static org.junit.Assert.*;
 public class KingTest {
 
     @Test
+    public void kingIsWhite_inputTrue_W() {
+        int inicialPocionX = 4;
+        int inicialPocionY = 4;
+        King king = new King(true, inicialPocionX, inicialPocionY);
+        char actual = king.getColor();
+        char expected = 'W';
+        assertEquals(expected, actual);
+    }
+
+    @Test
     public void getFigureKing_choseKing_K() {
         int inicialPocionX = 4;
         int inicialPocionY = 4;
@@ -15,5 +25,4 @@ public class KingTest {
         char expected = 'K';
         assertEquals(expected, actual);
     }
-
 }
