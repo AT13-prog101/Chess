4f0a375cb406e961d2475a6a625869e8f24d9021
Jorge Caceres <jcaceresvelasco@gmail.com>
2021-03-22 23:21:43 -0400

    Add unit test for move a piece

--- app/src/test/java/src/ChessboardTest.java
+++ app/src/test/java/src/ChessboardTest.java
@@ -1,7 +1,20 @@
 package src;
 
 import org.junit.Test;
+import src.Pieces.Piece;
+
 import static org.junit.Assert.*;
 
 public class ChessboardTest {
+    @Test
+    public void invalidMoveTest() {
+        Chessboard chessboard = new Chessboard();
+        Player playerWhite = new Player("Player1", true);
+        Position source = new Position("d1");
+        Position target = new Position("d2");
+        boolean expected= false;
+        boolean actual = chessboard.movePiece(source,target,playerWhite);
+        assertEquals(expected, actual);
+    }
 }
+
