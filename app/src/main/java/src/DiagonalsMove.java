e9092f7cd156ff7858f050f75b5ddf3053851c94
Gustavo Huanca <gustavozha@gmail.com>
2021-03-23 23:03:42 -0400

    Refactor DiagonalsMove

--- app/src/main/java/src/DiagonalsMove.java
+++ app/src/main/java/src/DiagonalsMove.java
@@ -40,14 +40,6 @@ public class DiagonalsMove implements MovePiece {
         return movePoints;
     }
 
-    public boolean isMoveValid(int xDest, int yDest) {
-        for (Position availableMove : movePoints) {
-            if (availableMove.getPosX() == xDest && availableMove.getPosY() == yDest)
-                return true;
-        }
-        return false;
-    }
-
     /**
      * Verify if the space of destination is free from piece of the same color
      *
@@ -58,31 +50,50 @@ public class DiagonalsMove implements MovePiece {
     public boolean isAccessiblePosition(int posY, int posX, boolean colorPiece) {
         boolean oppositeColor = !colorPiece;
 
-        if (Chessboard.board[posX][posY] == null) {
+        if(isSpaceEmpty(Chessboard.board[posX][posY])){
             movePoints.add(new Position(posY, posX));
             return true;
         }
-        if (Chessboard.board[posX][posY].getColorWhite() != colorPiece) {
+        if (isSpaceWithEnemy(Chessboard.board[posX][posY],colorPiece) ){
             movePoints.add(new Position(posY, posX));
             return false;
         }
-        if (Chessboard.board[posX][posY].getColorWhite() == colorPiece) {
-            return false;
-        }
         return false;
     }
+    /**
+     *  Verify if the space of destination is free from piece of the same color or is empty.
+     * @param piece is the piece in the possible position.
+     * @return true is space is empty or has a piece of different color, false if has piece of same color
+     */
+    public boolean isSpaceEmpty(Piece piece) {
+        if(piece == null)
+            return true;
+        return false;
+    }
+
+    /**
+     *  Verify if Destiny position has an enemy
+     * @param piece
+     * @param isWhite
+     * @return
+     */
+    public boolean isSpaceWithEnemy(Piece piece, boolean isWhite) {
+        if(piece.getColorWhite() != isWhite)
+            return true;
+        else
+            return false;
+    }
+
+    /**
+     *  Verify is the Position is inside the Limits of the Chessboard
+     * @param position
+     * @return true if its in the limits, false if not
+     */
     @Override
     public boolean isInLimits(int position) {
-        return false;
-    }
-
-    @Override
-    public boolean isSpaceWithEnemy(Piece piece, boolean isWhite) {
-        return false;
-    }
-
-    @Override
-    public boolean isSpaceEmpty(Piece piece) {
+        if(0 <= position && position < Chessboard.DIMENSION) {
+            return true;
+        }
         return false;
     }
 }
