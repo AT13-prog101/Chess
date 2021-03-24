5097237fe63c4f41a5c37ba911578287b2318579
Gustavo Huanca <gustavozha@gmail.com>
2021-03-24 15:56:22 -0400

    Add chessboardScenario method

--- app/src/test/java/src/Pieces/RookTest.java
+++ app/src/test/java/src/Pieces/RookTest.java
@@ -1,11 +1,46 @@
 package src.Pieces;
 
 import org.junit.Test;
+import src.Chessboard;
+import src.GetPieceFactory;
 import src.Position;
+import src.TypePiece;
+
+import java.util.List;
 
 import static org.junit.Assert.*;
 
 public class RookTest {
+
+    private void chessboardScenario() {
+        Chessboard chessboard = new Chessboard();
+        chessboard.board = new Piece[Chessboard.DIMENSION][Chessboard.DIMENSION];
+        GetPieceFactory getPieceFactory = new GetPieceFactory();
+        Chessboard.setPiece(getPieceFactory.getPiece(TypePiece.PAWN, false, new Position(2, 3)));
+        Chessboard.setPiece(getPieceFactory.getPiece(TypePiece.KING, true, new Position(3, 0)));
+        Chessboard.setPiece(getPieceFactory.getPiece(TypePiece.BISHOP, true, new Position(3, 5)));
+        /**
+         *   | ---------------------------------------
+         * 8 |    |    |    | WK |    |    |    |    |
+         *   |---------------------------------------
+         * 7 |    |    |    |    |    |    |    |    |
+         *   |---------------------------------------
+         * 6 |    |    |    |    |    |    |    |    |
+         *   |---------------------------------------
+         * 5 |    |    | BP |    |    |    |    |    |
+         *   |---------------------------------------
+         * 4 |    |    |    |    |    |    |    |    |
+         *   |---------------------------------------
+         * 3 |    |    |    | WB |    |    |    |    |
+         *   |---------------------------------------
+         * 2 |    |    |    |    |    |    |    |    |
+         *   |---------------------------------------
+         * 1 |    |    |    |    |    |    |    |    |
+         *   |---------------------------------------
+         *     a    b    c    d    e    f    g    h
+         */
+    }
+    
     @Test
     public void rookIsBlack_inputFalse_B() {
         int inicialPocionX = 8;
