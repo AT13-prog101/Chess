1d899b078f2e2419996a59ae8012d4d868015314
Gustavo Huanca <gustavozha@gmail.com>
2021-03-24 15:19:49 -0400

    Add rookIsWhite_inputTrue_W unit test in RookTest

--- app/src/test/java/src/Pieces/RookTest.java
+++ app/src/test/java/src/Pieces/RookTest.java
@@ -7,10 +7,21 @@ import static org.junit.Assert.*;
 
 public class RookTest {
 
+
+    @Test
+    public void rookIsWhite_inputTrue_W() {
+        int inicialPocionX = 8;
+        int inicialPocionY = 2;
+        Rook rook = new Rook(true, new Position(inicialPocionX, inicialPocionY));
+        char actual = rook.getColor();
+        char expected = 'W';
+        assertEquals(expected, actual);
+    }
+
     @Test
     public void getFigureRook_choseRook_R() {
         int inicialPocionX = 7;
-        int inicialPocionY = 0;
+        int inicialPocionY = 6;
         Rook rook = new Rook(true, new Position(inicialPocionX, inicialPocionY));
         char actual = rook.getFigure();
         char expected = 'R';
