625f2ca16b0e3d712fc21ffd1c6d63eeaf7185bb
Gustavo Huanca <gustavozha@gmail.com>
2021-03-24 15:14:21 -0400

    Add getFigureRook_choseRook_R

--- app/src/test/java/src/Pieces/RookTest.java
+++ app/src/test/java/src/Pieces/RookTest.java
@@ -1,12 +1,19 @@
 package src.Pieces;
 
 import org.junit.Test;
+import src.Position;
 
 import static org.junit.Assert.*;
 
 public class RookTest {
 
     @Test
-    public void getFigure() {
+    public void getFigureKing_choseBishop_B() {
+        int inicialPocionX = 7;
+        int inicialPocionY = 0;
+        Rook rook = new Rook(true, new Position(inicialPocionX, inicialPocionY));
+        char actual = rook.getFigure();
+        char expected = 'R';
+        assertEquals(expected, actual);
     }
 }
