fb550a03f93468778e914bac03015e9b1ad38e47
Gustavo Huanca <gustavozha@gmail.com>
2021-03-25 11:35:27 -0400

    Add queenIsWhite_InputTrue_W unit tests

--- app/src/test/java/src/Pieces/QueenTest.java
+++ app/src/test/java/src/Pieces/QueenTest.java
@@ -6,6 +6,15 @@ import src.Position;
 import static org.junit.Assert.*;
 
 public class QueenTest {
+    @Test
+    public void queenIsWhite_inputTrue_W() {
+        int inicialPocionX = 5;
+        int inicialPocionY = 6;
+        Queen queen = new Queen(true, new Position(inicialPocionX, inicialPocionY));
+        char actual = queen.getColor();
+        char expected = 'W';
+        assertEquals(expected, actual);
+    }
 
     @Test
     public void getFigureQueen_ChoseQueen_Q() {
