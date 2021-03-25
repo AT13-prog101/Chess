e48374fff7475f30897175f30029b9e81dd6ccba
Gustavo Huanca <gustavozha@gmail.com>
2021-03-25 11:25:26 -0400

    Add new getFigureQueen_ChoseQueen_Q in QueenTest

--- app/src/test/java/src/Pieces/QueenTest.java
+++ app/src/test/java/src/Pieces/QueenTest.java
@@ -1,12 +1,19 @@
 package src.Pieces;
 
 import org.junit.Test;
+import src.Position;
 
 import static org.junit.Assert.*;
 
 public class QueenTest {
 
     @Test
-    public void getFigure() {
+    public void getFigureQueen_ChoseQueen_Q() {
+        int inicialPocionX = 4;
+        int inicialPocionY = 3;
+        Queen queen = new Queen(true, new Position(inicialPocionX, inicialPocionY));
+        char actual = queen.getFigure();
+        char expected = 'Q';
+        assertEquals(expected, actual);
     }
 }
