5360207e820bce406d68048ed28d14f03f6efb8f
Gustavo Huanca <gustavozha@gmail.com>
2021-03-25 10:28:03 -0400

    Fix spelling errors in RookTest

--- app/src/test/java/src/Pieces/RookTest.java
+++ app/src/test/java/src/Pieces/RookTest.java
@@ -41,11 +41,11 @@ public class RookTest {
          */
     }
     @Test
-    public void getValidMovesBishopWhite_a8_nineValidMoves() {
+    public void getValidMovesBishopWhite_A8_NineValidMoves() {
         chessboardScenario();
         GetPieceFactory getPieceFactory = new GetPieceFactory();
         Chessboard.setPiece(getPieceFactory.getPiece(TypePiece.ROOK, true, new Position(0, 0)));
-        Rook rook = (Rook) Chessboard.board[0][0];
+        Piece rook = Chessboard.board[0][0];
         List<Position> validMoves = rook.getValidMoves();
         String expected = "b8 c8 a7 a6 a5 a4 a3 a2 a1 ";
         String actual = "";
@@ -56,11 +56,11 @@ public class RookTest {
     }
 
     @Test
-    public void getValidMovesBishopWhite_h1_fourteenValidMoves() {
+    public void getValidMovesBishopWhite_H1_FourteenValidMoves() {
         chessboardScenario();
         GetPieceFactory getPieceFactory = new GetPieceFactory();
         Chessboard.setPiece(getPieceFactory.getPiece(TypePiece.ROOK, true, new Position(7, 7)));
-        Rook rook = (Rook) Chessboard.board[7][7];
+        Piece rook = Chessboard.board[7][7];
         List<Position> validMoves = rook.getValidMoves();
         String expected = "g1 f1 e1 d1 c1 b1 a1 h2 h3 h4 h5 h6 h7 h8 ";
         String actual = "";
@@ -71,11 +71,11 @@ public class RookTest {
     }
 
     @Test
-    public void getValidMovesBishopBlack_h8_elevenValidMoves() {
+    public void getValidMovesBishopBlack_H8_ElevenValidMoves() {
         chessboardScenario();
         GetPieceFactory getPieceFactory = new GetPieceFactory();
         Chessboard.setPiece(getPieceFactory.getPiece(TypePiece.ROOK, false, new Position(7, 0)));
-        Rook rook = (Rook) Chessboard.board[0][7];
+        Piece rook = Chessboard.board[0][7];
         List<Position> validMoves = rook.getValidMoves();
         String expected = "g8 f8 e8 d8 h7 h6 h5 h4 h3 h2 h1 ";
         String actual = "";
@@ -86,11 +86,11 @@ public class RookTest {
     }
 
     @Test
-    public void getValidMovesBishopBlack_a1_fourteenValidMoves() {
+    public void getValidMovesBishopBlack_A1_FourteenValidMoves() {
         chessboardScenario();
         GetPieceFactory getPieceFactory = new GetPieceFactory();
         Chessboard.setPiece(getPieceFactory.getPiece(TypePiece.ROOK, false, new Position(0, 7)));
-        Rook rook = (Rook) Chessboard.board[7][0];
+        Piece rook = Chessboard.board[7][0];
         List<Position> validMoves = rook.getValidMoves();
         String expected = "b1 c1 d1 e1 f1 g1 h1 a2 a3 a4 a5 a6 a7 a8 ";
         String actual = "";
@@ -100,11 +100,11 @@ public class RookTest {
         assertEquals(expected, actual);
     }
     @Test
-    public void getValidMovesBishopBlack_d5_eightValidMoves() {
+    public void getValidMovesBishopBlack_D5_EightValidMoves() {
         chessboardScenario();
         GetPieceFactory getPieceFactory = new GetPieceFactory();
         Chessboard.setPiece(getPieceFactory.getPiece(TypePiece.ROOK, false, new Position(3, 3)));
-        Rook rook = (Rook) Chessboard.board[3][3];
+        Piece rook = Chessboard.board[3][3];
         List<Position> validMoves = rook.getValidMoves();
         String expected = "e5 f5 g5 h5 d4 d3 d6 d7 d8 ";
         String actual = "";
@@ -114,7 +114,7 @@ public class RookTest {
         assertEquals(expected, actual);
     }
     @Test
-    public void rookIsBlack_inputFalse_B() {
+    public void rookIsBlack_InputFalse_B() {
         int inicialPocionX = 8;
         int inicialPocionY = 2;
         Rook rook = new Rook(true, new Position(inicialPocionX, inicialPocionY));
@@ -124,7 +124,7 @@ public class RookTest {
     }
 
     @Test
-    public void rookIsWhite_inputTrue_W() {
+    public void rookIsWhite_InputTrue_W() {
         int inicialPocionX = 8;
         int inicialPocionY = 2;
         Rook rook = new Rook(true, new Position(inicialPocionX, inicialPocionY));
@@ -134,7 +134,7 @@ public class RookTest {
     }
 
     @Test
-    public void getFigureRook_choseRook_R() {
+    public void getFigureRook_ChoseRook_R() {
         int inicialPocionX = 7;
         int inicialPocionY = 6;
         Rook rook = new Rook(true, new Position(inicialPocionX, inicialPocionY));
