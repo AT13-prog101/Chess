ba206ba4c51247cd554602f1b63a77b90546cd8a
SaulCM <saulcaspamiranda@gmail.com>
2021-03-25 22:44:17 -0400

    add documentation for castling moves in chessboard

--- app/src/main/java/src/Chessboard.java
+++ app/src/main/java/src/Chessboard.java
@@ -46,6 +46,11 @@ public class Chessboard {
         }
         System.out.println("    a    b    c    d    e    f    g    h");
     }
+
+    /**
+     * Checks if a piece can be moved in the board while checking if the move is
+     * castling or the king has been taken
+     */
     public boolean movePiece(Position source, Position target, Player player) {
         List<Position> validMoves = getValidMoves(source, player);
         if (validMoves.contains(target)) {
@@ -63,17 +68,25 @@ public class Chessboard {
         }
         return false;
     }
+
+    /**
+     * Update the piece on the chessboard given a source and target position.
+     */
     public void moveFromSourceToTarget(Position source, Position target) {
         Piece pieceToMove = board[source.getPosY()][source.getPosX()];
         pieceToMove.updatePosition(new Position(target.getPosX(), target.getPosY()));
         Chessboard.board[target.getPosY()][target.getPosX()] = pieceToMove;
         Chessboard.board[source.getPosY()][source.getPosX()] = null;
     }
+
+    /**
+     * Returns the player who made the move to take the king
+     */
     public Player kingTakenBy() {
         return winner;
     }
     /**
-     * Returns all the valid moves a piece from the given source position can make
+     * Returns all the valid moves a piece from the given source position can make.
      */
     public List<Position> getValidMoves(Position source, Player player) {
         List<Position> validMoves = new ArrayList<Position>();
@@ -87,6 +100,10 @@ public class Chessboard {
             return validMoves;
         }
     }
+
+    /**
+     * Checks if a valid castling move has been made and makes the move.
+     */
     public boolean checkCastlingMove(Position source, Position target, List<Position> validMoves) {
         int posXCastling = DIMENSION;
         int posXRookSource = DIMENSION;
@@ -122,6 +139,10 @@ public class Chessboard {
             System.out.println(pos.getCharAlg() + " ");
         }
     }
+
+    /**
+     * Checks if there is a piece in the chessboard in the given position
+     */
     public boolean thereIsPiece (Position position) {
         if (board[position.getPosY()][position.getPosX()] == null)
             return false;
